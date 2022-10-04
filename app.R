0a6df92765dcfa07953e54ec1467d0fab914d7b7
Izote <munguiamv@gmail.com>
2022-10-03 22:19:12 -0400

    placeholder layout

--- app.R
+++ app.R
@@ -1,13 +1,23 @@
 library(shiny)
 
 server <- function(input, output) {
-  output$dist_plot <- renderPlotly({dist_plot("2A")})
+  output$dist_plot <- renderPlotly({dist_plot(input$pid_selection)})
 }
 
 ui <- fluidPage(
-  mainPanel(
-    plotlyOutput("dist_plot")
+  title = "Browser Title",
+  fluidRow(
+    column(
+      width = 4,
+      h1("Visual Title"),
+      selectInput("pid_selection", "Label", parameters$id)
+      ),
+    # Placeholder columns, assuming additional controls.
+    column(width = 4),
+    column(width = 4),
+    ),
+  hr(),
+  plotlyOutput("dist_plot")
   )
-)
 
 shinyApp(ui = ui, server = server)
