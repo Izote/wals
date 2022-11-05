0ae065463a2677104dd10390e657ae43dbf26401
Izote <munguiamv@gmail.com>
2022-11-05 15:20:59 -0400

    refactors + ui update

--- app.R
+++ app.R
@@ -1,31 +1,35 @@
 library(shiny)
 
 server <- function(input, output) {
-  output$title <- renderText({
-    sprintf("Now viewing: %s", get_parameter(input$param)$name)
+  output$link_button <- renderUI({
+    a(
+      "View related WALS chapter",
+      class = "btn btn-default",
+      href = sprintf(
+        "https://wals.info/chapter/%s",
+        str_remove_all(input$param, "[A-Z]")
+        ),
+      target = "_blank"
+      )
+  })
+  
+  output$bar_plot <- renderPlotly({
+    bar_plot(input$param)
     })
   
-  output$macro_plot <- renderPlotly({
-    macro_plot(input$param)
-    })
-  
-  output$family_plots <- renderPlotly({
-    family_plots(input$param)
+  output$heat_maps <- renderPlotly({
+    heat_maps(input$param)
     })
 }
 
 ui <- fluidPage(
-  title = "Browser Title",
-  fluidRow(
-    textOutput("title", container = h2), # manage size with CSS
-    column(
-      width = 12,
-      selectInput("param", "Parameter", param_choices),
-      ),
-    ),
+  title = "WALS Summary View",
+  h1("WALS Summary View"),
+  selectInput("param", "WALS Feature/Parameter", param_choices),
+  uiOutput("link_button"),
+  plotlyOutput("bar_plot"),
   hr(),
-  plotlyOutput("macro_plot"),
-  plotlyOutput("family_plots"),
+  plotlyOutput("heat_maps")
   )
 
 shinyApp(ui = ui, server = server)
