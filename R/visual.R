c26af4211e613644176e66aea09502d99a09396d
Izote <munguiamv@gmail.com>
2022-11-05 15:20:46 -0400

    refactors + visual improvements

--- R/visual.R
+++ R/visual.R
@@ -1,6 +1,6 @@
 library(plotly)
 
-macro_plot <- function(pid, plot_type = "bar") {
+bar_plot <- function(pid, plot_type = "bar") {
   param <- get_parameter(pid)
   
   param %>% 
@@ -9,33 +9,59 @@ macro_plot <- function(pid, plot_type = "bar") {
       x = ~ macroarea, y = ~ percent,
       color = ~ label,
       type = plot_type
+      ) %>%
+    add_trace(
+      hovertemplate = "%{x}: %{y:.0%}<extra></extra>"
       ) %>% 
     layout(
-      title = param$name
+      plot_bgcolor="#CCCCCC",
+      title = "Parameter distribution by linguistic macroarea",
+      xaxis = list(title = NA_character_),
+      yaxis = list(title = NA_character_),
+      legend = list(orientation = "h")
     )
 }
 
-family_plot <- function(df) {
+heat_map <- function(df) {
   df %>% 
     drop_na() %>% 
     plot_ly(
       x = ~ family, y = ~ label, z = ~ percent,
-      type = "heatmap", colors = "Greys",
-      showscale = FALSE
+      type = "heatmap",
+      zauto = FALSE,
+      zmin = 0.0, zmax = 1.0,
+      coloraxis = "coloraxis",
+      hoverongaps = FALSE
+    ) %>% 
+    add_trace(
+      hovertemplate = "%{x}: %{z:.0%}<extra></extra>"
     ) %>% 
     layout(
-      xaxis = list(tickangle = -45)
+      plot_bgcolor="#CCCCCC",
+      coloraxis = list(colorscale = list(
+        list(NA, "#CCCCCC"),
+        list(0, "#00353D"),
+        list(1, "#5DE9FE")
+      )),
+      xaxis = list(
+        title = NA_character_,
+        tickangle = -45
+        ),
+      yaxis = list(
+        title = NA_character_
+      )
     )
 }
 
-family_plots <- function(pid) {
+heat_maps <- function(pid) {
   values <- get_parameter(pid) %>% 
     count_values(c("macroarea", "family")) %>% 
     group_by(macroarea) %>% 
-    do(fig = family_plot(.))
+    do(fig = heat_map(.))
   
   fig <- values %>% 
-    subplot(shareY = TRUE, nrows = 1)
+    subplot(shareY = TRUE, nrows = 1) %>% 
+    layout(title = "Parameter distribution by language family")
   
   fig
 }
